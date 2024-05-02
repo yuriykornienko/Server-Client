36173dcec919cd11d50dca9e063fd670ef67aad8
yuriy <yuriykornienko777@gmail.com>
2024-05-02 09:29:28 +0300

    Добавьте файлы проекта.

---
+++ NetServer2/SimpleServer2.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <old_net.h>
+
+enum class CustomMsgTypes : uint32_t
+{
+	ServerAccept,
+	ServerDeny,
+	ServerPing,
+	MessageAll,
+	ServerMessage,
+};
+
+
+
+class CustomServer : public olc::net::server_interface<CustomMsgTypes>
+{
+public:
+	CustomServer(uint16_t nPort) : olc::net::server_interface<CustomMsgTypes>(nPort)
+	{
+
+	}
+
+protected:
+	virtual bool OnClientConnect(std::shared_ptr<olc::net::connection<CustomMsgTypes>> client)
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::ServerAccept;
+		client->Send(msg);
+		return true;
+	}
+
+	//Вызывается, когда кажется, что клиент отключился
+	virtual void OnClientDisconnect(std::shared_ptr<olc::net::connection<CustomMsgTypes>> client)
+	{
+		std::cout << "Removing client [" << client->GetID() << "]\n";
+	}
+
+	//Вызывается при поступлении сообщения
+	virtual void OnMessage(std::shared_ptr<olc::net::connection<CustomMsgTypes>> client, olc::net::message<CustomMsgTypes>& msg)
+	{
+		switch (msg.header.id)
+		{
+		case CustomMsgTypes::ServerPing:
+		{
+			std::cout << "[" << client->GetID() << "]: Server Ping\n";
+
+			// Просто отправьте сообщение обратно клиенту
+			client->Send(msg);
+		}
+		break;
+
+		case CustomMsgTypes::MessageAll:
+		{
+			std::cout << "[" << client->GetID() << "]: Message All\n";
+
+			//Создайте новое сообщение и отправьте его всем клиентам
+			olc::net::message<CustomMsgTypes> msg;
+			msg.header.id = CustomMsgTypes::ServerMessage;
+			msg << client->GetID();
+			MessageAllClients(msg, client);
+
+		}
+		break;
+		}
+	}
+};
+
+int main()
+{
+	CustomServer server(60000);
+	server.Start();
+
+	while (1)
+	{
+		server.Update(-1, true);
+	}
+
+
+
+	return 0;
+}
